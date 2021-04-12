76fc7e795f7cbef2220d20fecae341ecb5bdc7f4
Jon Hanson <joha7866@colorado.edu>
2021-04-11 22:10:45 -0600

    Initial commit of align behavior

---
+++ align.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''This program attempts to legally navigate the course and lock a target.
+
+It is a fundamental behavior program that:
+ - Implements the RGB, Pixy, and US sensors
+ - Actuates the motors
+ - Attempts to navigate the course legally (but not necessarily logically)
+'''
+import time
+import board
+import busio
+import adafruit_tca9548a
+import adafruit_tcs34725
+import adafruit_hcsr04
+from adafruit_bus_device.i2c_device import I2CDevice
+
+#i2c addressses
+MOTOR_CTRL_ADDR = 0x69
+PIXY_ADDR = 0x54
+MAX_I2C_MSG_BYTES = 16
+
+#gpio definitions
+LEFT_ULTRASONIC_PAIR = [19,26]
+RIGHT_ULTRASONIC_PAIR = [16,20]
+
+if __name__ == "__main__":
+    with busio.I2C(board.SCL, board.SDA) as bus:
+        mux = adafruit_tca9548a.TCA9548A(bus)
