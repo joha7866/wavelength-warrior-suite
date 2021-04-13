7c12fe34afeeffd2d306d85b97efd44e58b56063
Jon Hanson <joha7866@colorado.edu>
2021-04-12 20:09:49 -0600

    Updating align to use correct mux addrs and work better with keyboard interrupt

--- align.py
+++ align.py
@@ -26,20 +26,23 @@ LEFT_ULTRASONIC_PAIR = [19,26]
 RIGHT_ULTRASONIC_PAIR = [16,20]
 
 if __name__ == "__main__":
-    read_buff = bytearray(16)
-    loop_count = 0
-    with busio.I2C(board.SCL, board.SDA) as bus:
-        mux = adafruit_tca9548a.TCA9548A(bus)
-        rgb_left = adafruit_tcs34725.TCS34725(mux[7])
-        rgb_right = adafruit_tcs34725.TCS34725(mux[5])
-        motor = I2CDevice(mux[4], MOTOR_CTRL_ADDR, probe=False)
-        try:
+    try:
+        read_buff = bytearray(16)
+        loop_count = 0
+        STATE = 'TRAVELLING'
+
+        with busio.I2C(board.SCL, board.SDA) as bus:
+            mux = adafruit_tca9548a.TCA9548A(bus)
+            rgb_left = adafruit_tcs34725.TCS34725(mux[5])
+            rgb_right = adafruit_tcs34725.TCS34725(mux[7])
+            motor = I2CDevice(mux[0], MOTOR_CTRL_ADDR, probe=False)
+
             while 1:
                 #read rgbs
                 lux_l = rgb_left.lux
                 lux_r = rgb_right.lux
-                left_edge = lux_l>900
-                right_edge = lux_r>900
+                left_edge = lux_l>800
+                right_edge = lux_r>800
                 # if loop_count%10 == 0:
                 #     print(f'Lux L:{lux_l:.1f}, R:{lux_r:.1f}')
 
@@ -60,8 +63,8 @@ if __name__ == "__main__":
                     time.sleep(5)
 
                 loop_count += 1
-        except KeyboardInterrupt:
-            with motor:
-                motor.write_then_readinto(motor_lib.STOP_CMD, read_buff)
-            print('exited gracefully')
+    except KeyboardInterrupt:
+        with motor:
+            motor.write_then_readinto(motor_lib.STOP_CMD, read_buff)
+        print('exited gracefully')
 
