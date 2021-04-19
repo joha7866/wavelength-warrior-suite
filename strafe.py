59907d2efc7fc016b08f1223a64454b747fba5b8
Jon Hanson <joha7866@colorado.edu>
2021-04-18 20:01:40 -0600

    Updated strafe to use color and lux triggers for intersection crossing

--- strafe.py
+++ strafe.py
@@ -36,24 +36,40 @@ if __name__ == "__main__":
         rgb_right = adafruit_tcs34725.TCS34725(mux[7])
         motor = I2CDevice(mux[0], MOTOR_CTRL_ADDR, probe=False)
 
+        lux_l = rgb_left.lux
+        lux_r = rgb_right.lux
+        time.sleep(0.5)
+
         with motor:
             motor.write_then_readinto(motor_lib.FORWARD_CMD, read_buff)
         active_cmd = chr(read_buff[0])
 
         while 1:
             #read rgbs
+            rl, gl, bl = rgb_left.color_rgb_bytes
             lux_l = rgb_left.lux
+            rr, gr, br = rgb_right.color_rgb_bytes
             lux_r = rgb_right.lux
-            left_edge = lux_l>250
-            right_edge = lux_r>250
+
+            left_purple = 1200>=lux_l>400 and rl>30
+            right_purple = 1200>=lux_r>400 and rr>30
+            left_yellow = lux_l>1200
+            right_yellow = lux_r>1200
             # if loop_count%200 == 0:
             #     print(f'Lux L:{lux_l:.1f}, R:{lux_r:.1f}')
             #     print(f'State: {STATE}')
-            if left_edge or right_edge:
-                hit_count += 1
-                time.sleep(0.25)
-            if hit_count >= 2:
+            if left_yellow or right_yellow:
+                print('stopped on edge')
                 with motor:
                     motor.write_then_readinto(motor_lib.STOP_CMD, read_buff)
                 active_cmd = chr(read_buff[0])
                 sys.exit()
+
+            if left_purple or right_purple:
+                hit_count += 1
+                if hit_count >= 2:
+                    with motor:
+                        motor.write_then_readinto(motor_lib.STOP_CMD, read_buff)
+                    active_cmd = chr(read_buff[0])
+                    sys.exit()
+                time.sleep(0.4)
